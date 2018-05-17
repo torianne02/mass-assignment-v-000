e22cb9888184e2c2b1f9662f6fd4a9cfd8b4afe9
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-05-17 19:55:14 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,7 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
 end
